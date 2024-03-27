ba7e3c696bbe48f9031255834ed5136a40233f4e
Ryan Tatum <155933226+RTgotthepower@users.noreply.github.com>
2024-03-27 12:15:50 -0500

    seeing something

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// You can add JavaScript functionality here
-// For example, to create a slideshow in the gallery section:
-const images = document.querySelectorAll('.gallery img');
-let current = 0;
-
-function nextSlide() {
-    images[current].style.display = 'none';
-    current = (current + 1) % images.length;
-    images[current].style.display = 'block';
+window.onload = function() {
+    document.getElementById('year').innerHTML = new Date().getFullYear();
 }
-
-setInterval(nextSlide, 3000); // Change image every 3 seconds
