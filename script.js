a8e3100212cf179abb48ed447cd5d7bda3f3f0d8
Ryan Tatum <155933226+RTgotthepower@users.noreply.github.com>
2024-03-27 12:08:29 -0500

    samething

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function main(){
-    console.log("Hello World");
-    
+// You can add JavaScript functionality here
+// For example, to create a slideshow in the gallery section:
+const images = document.querySelectorAll('.gallery img');
+let current = 0;
+
+function nextSlide() {
+    images[current].style.display = 'none';
+    current = (current + 1) % images.length;
+    images[current].style.display = 'block';
 }
 
-main();
+setInterval(nextSlide, 3000); // Change image every 3 seconds
