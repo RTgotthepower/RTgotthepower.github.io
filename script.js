0778483a5478d3cf874069393f0c204834db3761
Ryan Tatum <155933226+RTgotthepower@users.noreply.github.com>
2024-04-11 21:24:52 -0500

    correct

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
